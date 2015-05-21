393e871815576e08ec846c8033684605af9ad174
Luis Recchini <luisrecchini@gmail.com>
2015-05-20 23:52:11 -0300

    custom_error_handler

--- php/config/config.php
+++ php/config/config.php
@@ -2,6 +2,7 @@
 if (!isset($_SESSION)) {
 	session_start();
 }
+
 define('ENTORNO', 'DEV');
 define('SALT', 'c0ncurs4nd0!');
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
@@ -10,3 +11,13 @@ define("CONTROLLERS_PATH", ROOT.'/php/controllers/');
 define("MODELS_PATH", ROOT.'/php/models/');
 define("VIEWS_PATH", ROOT.'/php/views/');
 define("CONFIG_PATH", ROOT.'/php/config/');
+function custom_error_handler($error_number, $error_string, $error_file, $error_line, $error_context) {
+	require_once(MODELS_PATH.'Log.php');
+	$data_error['file'] = $error_file;
+	$data_error['line'] = $error_line;
+	$data_error['context'] = $error_context;
+	$data_error['message'] = $error_string;
+	$data_error['errno'] = $error_number;
+	Log::error($data_error);
+}
+set_error_handler("custom_error_handler", E_ALL);
--- php/models/Log.php
+++ php/models/Log.php
@@ -17,42 +17,51 @@ class Log {
         
     }
     
-    private function formatData($data){
+    private static function formatData($data){
         
         $str = "[" . date('Y-m-d H:i:s') . " / " . $_SERVER['REMOTE_ADDR'] . "]\n";
-        foreach($data as $key => $value){
-            $str .= "$key: $value\n";
-        }
+		if (is_array($data)) {
+			foreach($data as $key => $value){
+				if (!is_array($value)) {
+					$str .= "$key: $value\n";
+				} else {
+					$str.= "$key: ".json_encode($value)."\n";
+				}
+			}
+		} else {
+			$str.= $data."\n";
+		}
         $str .= "-----------------------------------------------------------------------------\n\n";
         return $str;
     }
     
     public static function error($data) {
-        file_put_contents(ROOT . '/logs/error.txt', $this->form($data), FILE_APPEND);
+        file_put_contents(ROOT . '/logs/error.txt', self::formatData($data), FILE_APPEND);
     }
 
     public static function form($data) {
-        file_put_contents(ROOT . '/logs/register_form.txt', $this->form($data), FILE_APPEND);
+        file_put_contents(ROOT . '/logs/register_form.txt', self::formatData($data), FILE_APPEND);
     }
 
     public static function base($data) {
-        file_put_contents(ROOT . '/logs/db.txt', $this->form($data), FILE_APPEND);
+        file_put_contents(ROOT . '/logs/db.txt', self::formatData($data), FILE_APPEND);
     }
 
     public static function confirmacionMail($data) {
-        file_put_contents(ROOT . '/logs/invalid-token.txt', $this->form($data), FILE_APPEND);
+        file_put_contents(ROOT . '/logs/invalid-token.txt', self::formatData($data), FILE_APPEND);
     }
 
     public static function userLogin($data) {
-        file_put_contents(ROOT . '/logs/user-login.txt', $this->form($data), FILE_APPEND);
+        file_put_contents(ROOT . '/logs/user-login.txt', self::formatData($data), FILE_APPEND);
     }
 
     public static function recovery($data) {
-        file_put_contents(ROOT . '/logs/recovery.txt', $this->form($data), FILE_APPEND);
+        file_put_contents(ROOT . '/logs/recovery.txt', self::formatData($data), FILE_APPEND);
     }
 
     public static function devMail($data) {
-        file_put_contents(ROOT . '/logs/dev_mail.txt', $this->form($data), FILE_APPEND);
+        file_put_contents(ROOT . '/logs/dev_mail.txt', self::formatData($data), FILE_APPEND);
     }
+	
 
 }
