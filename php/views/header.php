9b0134d7798081924b76ff94ed05ed4991f0440b
h2swider <lucasswider@hotmail.com>
2015-06-04 00:18:03 -0300

    Nueva fuente

--- php/views/header.php
+++ php/views/header.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="<?php echo ASSETS_PATH; ?>images/favicon.png" type="image/png">
         <link rel="shortcut icon" href="<?php echo ASSETS_PATH; ?>images/favicon.png" type="image/png"> 
+        <link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
         <link href="<?php echo ASSETS_PATH; ?>css/bootstrap.min.css" rel="stylesheet"/>
         <link href="<?php echo ASSETS_PATH; ?>css/bootstrap-theme.min.css" rel="stylesheet"/>
         <link href="<?php echo ASSETS_PATH; ?>css/font-awesome.min.css" rel="stylesheet"/>
